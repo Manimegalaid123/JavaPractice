de7f9dd5f50a7d3bc80f01384a6bbcd7e991f188
manimegalai <manimegalaid.23cse@kongu.edu>
2026-02-07 10:55:56 +0530

    task

--- src/CountheNumber.java
+++ src/CountheNumber.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class CountheNumber {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:arr) {
+            if (map.containsKey(num)){
+                map.put(num,map.get(num)+1);
+            }else{
+                map.put(num,1);
+            }
+        }
+        for(int num:map.keySet()) {
+            System.out.println(num +" - "+map.get(num));
+        }
+    }
 }
--- src/PalindromeString.java
+++ src/PalindromeString.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class PalindromeString {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String:");
+        String s=sc.nextLine();
+        int i=0;
+        int j=s.length()-1;
+        boolean isPalindrome=true;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                isPalindrome=false;
+            }
+            i++;
+            j--;
+        }
+        if (isPalindrome) {
+
+            System.out.println("the String is Palindorme");
+        }
+        else{
+            System.out.println("The string is Not palindrome");
+        }
+    }
 }
